3b1232ebdb0da739506c85da5caa5fa6e04b1bd4
Jose Navarro <jfnr398@gmail.com>
2025-06-10 14:42:24 -0500

    chore: se añade origen (servidor local React + Vite) a configuración CORS

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://cesde-academic.netlify.app"],
+    allow_origins=["https://cesde-academic.netlify.app", "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
